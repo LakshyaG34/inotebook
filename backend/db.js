443147a6f65b0d2f089433601f4668f97b9d9bf8
Lakshya Goyal <lakshyagoyal34@gmail.com>
2024-01-26 23:18:23 +0530

    connectmongo

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017"
 
-
-const connectTomongo = () =>
-{
-    mongoose.connect(mongoURI, ()=>
-    {
-        console.log("connected to mongo succesfully")
-    }) //The arrow function inside mongoose.connect is called call-back function and the same can be done using async-await function
-}
+const connectTomongo = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/iNotebook', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
 
 module.exports = connectTomongo;
+
+
+
+//earlier I tried conncting to mongo using call back function but the newer version does niot connect to mongo using arrow function so I connected it usiong await/await syntax
+//async/await is a syntax that deals with promises
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const connectTomongo = require('./db');
-
+const express = require('express');
 connectTomongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello lakshya')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
